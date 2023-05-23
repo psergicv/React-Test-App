ca8c496f44ffbc0ce4e07eb2859bdeb00cbe9e81
psergicv <sv.postolachi@gmail.com>
2023-05-23 14:43:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import "../assets/style.css"
+
+function Nav(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Page 1</li>
+                <li>Page 2</li>
+                <li>Page 3</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+
+export default Nav;
